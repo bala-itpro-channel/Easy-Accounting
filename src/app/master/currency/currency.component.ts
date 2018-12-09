3bc0963fc665f66aea0161aa883adc2b537efd08
Bala <bala.in@gmail.com>
2018-12-09 18:55:19 -0500

    currency update

--- src/app/master/currency/currency.component.ts
+++ src/app/master/currency/currency.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./currency.component.scss']
 })
 export class CurrencyComponent implements OnInit {
-
+  transactions: Array<any> = [];
   constructor() { }
 
   ngOnInit() {
+    this.transactions = [
+      {
+        date: new Date(2017, 10, 10, 13, 10, 15),
+        label: 'Third transaction',
+        amount: 130
+      },
+      {
+        date: new Date(2017, 7, 3, 9, 35, 0),
+        label: 'Second transaction',
+        amount: 130
+      },
+      {
+        date: new Date(2017, 3, 27, 15, 43, 10),
+        label: 'First transaction',
+        amount: 130
+      }
+    ];
+
   }
 
 }
