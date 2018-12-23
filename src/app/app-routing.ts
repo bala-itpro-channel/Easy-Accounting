22b08e2436881bfe97bd3161338355cc7ec22c33
Bala Ganesan IT pro <bganesan@sonicad.org>
2018-12-23 18:27:35 -0500

    code clean

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -6,11 +6,6 @@ export const AppRoutes = [
         pathMatch: 'full',
         component:   HomeComponent
     },  
-    // {
-    //     path: 'master',
-    //     pathMatch: 'full',
-    //     loadChildren: './master/master.module#MasterModule'
-    // },
     {
         path: '',
         pathMatch: 'full',
@@ -21,34 +16,3 @@ export const AppRoutes = [
         redirectTo: 'home'
     },
 ];
-
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
-// import { AppComponent } from './app.component';
-// import { JournalComponent } from './gl/journal/journal.component';
-// import { CurrencyComponent } from './master/currency/currency.component';
-// import { LocationComponent } from './master/location/location.component';
-// import { HomeComponent } from './home/home.component';
-
-// const routes: Routes = [
-//   {
-//     path: 'home',
-//     component: HomeComponent
-//   },
-//   {
-//       path: '',
-//       redirectTo: '/home',
-//       pathMatch: 'full'
-//   },
-//   {
-//       path: '**',
-//       redirectTo: '/home',
-//       pathMatch: 'full'
-//   }
-// ];
-
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
