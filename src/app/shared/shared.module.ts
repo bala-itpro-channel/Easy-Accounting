dc276f5e2fe81c373e547dfc7f680fc3d306fa51
Bala Ganesan IT pro <bganesan@sonicad.org>
2018-12-24 08:51:53 -0500

    shared module added

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { DeleteDialogComponent } from './dialog/delete/delete.component';
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  declarations: [
+    DeleteDialogComponent
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: [],
+  exports: [DeleteDialogComponent],
+  entryComponents: [DeleteDialogComponent]
+})
+export class SharedModule { }
