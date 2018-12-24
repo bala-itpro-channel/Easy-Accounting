bff608b9e840eb4c6c1d138c2369c98ca111a4a4
Bala Ganesan IT pro <bganesan@sonicad.org>
2018-12-24 08:02:25 -0500

    spelling changed to loadChildren

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -10,7 +10,7 @@ export const AppRoutes = [
     },
     {
         path: 'master',
-        loadChilderen: './master/master.module#MasterModule'
+        loadChildren: './master/master.module#MasterModule'
     },
     {
         path: '',
