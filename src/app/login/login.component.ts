c0e20a4059c0194035c4d8b43cfe78a6045bb6e4
bala.itpro.channel <bala.itpro.channel@gmail.com>
2019-09-13 13:19:32 -0400

    code refactor

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AppService } from 'dist/AccountsProject/images/app/app.service';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-login',
